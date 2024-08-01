1e73d9c6cfb777a5bf511db239fa182e2a21cab6
flintstoneobed <okquartey@st.ug.edu.gh>
2024-08-01 12:19:35 +0000

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
+'''Task 0's module.
+'''
+
 
 def add(a: float, b: float) -> float:
-    """
-    Returns the sum of two floats.
-
-    Args:
-        a (float): The first float number.
-        b (float): The second float number.
-
-    Returns:
-        float: The sum of a and b.
-    """
+    '''Adds two floating-point numbers.
+    '''
     return a + b
